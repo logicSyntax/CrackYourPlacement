198f385aaa9089a8543b8fb15b38715db1d62af2
logicSyntax <singhaniya543@gmail.com>
2024-07-20 12:32:54 +0530

    [11] [Time Beats: 11.31%] [Memory Beats: 13.58%] - LeetPush

---
+++ 11-Container-With-Most-Water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int left = 0;
+        int right = height.size()-1;
+
+        while(left < right) {
+            ans = max(ans, (right-left)*min(height[left], height[right]));
+
+            // if(height[left] > height[right])
+            //     right--;
+            // else 
+            //     left++;
+
+            height[left] > height[right] ? right-- : left++;
+        }
+
+        return ans;
+    }
+};
