3de9a3713e87d63bf3431f392fc6fd13ff22483c
logicSyntax <singhaniya543@gmail.com>
2024-07-20 10:52:41 +0530

    [122] [Time Beats: 61.10%] [Memory Beats: 38.28%] - LeetPush

---
+++ 122-Best-Time-to-Buy-and-Sell-Stock-II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> curr(2, 0), ahead(2, 0);
+
+        for(int i=n-1; i>=0; i--) {
+            curr[1] = max(-prices[i] + ahead[0], ahead[1]);
+            curr[0] = max(prices[i] + ahead[1], ahead[0]);
+
+            ahead = curr;
+        } 
+        return ahead[1];
+    }
+};
