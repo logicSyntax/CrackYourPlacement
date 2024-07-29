f5164c92693d23aa29061eee6d3df1d8112d22d4
logicSyntax <singhaniya543@gmail.com>
2024-07-29 21:22:39 +0530

    [28] [Time Beats: 100.00%] [Memory Beats: 36.98%] - LeetPush

---
+++ 28-Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for (int i = 0; i < haystack.length(); i++) {
+            bool flag = false;
+            if (haystack[i] == needle[0]) {
+                for (int j = 0; j < needle.length(); j++) {
+                    if (haystack[i + j] != needle[j]) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) return i;
+            }
+        }
+        return -1;
+    }
+};
