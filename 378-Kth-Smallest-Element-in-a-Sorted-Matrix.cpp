4e47827b83d97510874797cb9f55f28c79225513
logicSyntax <singhaniya543@gmail.com>
2024-08-07 19:25:46 +0530

    [378] [Time Beats: 26.99%] [Memory Beats: 66.86%] - LeetPush

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix.cpp
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> pq;
-        for(int i=0; i<matrix.size(); i++) {
-            for(int j=0; j<matrix[0].size(); j++) {
-                pq.push(matrix[i][j]);
+        struct Compare {
+            bool operator()(pair<int, pair<int,int>> &a, pair<int, pair<int,int>> &b) {
+                return a.first > b.first;
+            }
+        };
 
-                if(pq.size() > k) {
-                    pq.pop();
-                }
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int,pair<int,int>>>, Compare> pq;
+        int n = matrix.size();
+
+        for(int i=0; i<n; i++) {
+            pq.push({matrix[i][0], {i, 0}});
+        }
+
+        int result = 0;
+        for(int i=0; i<k; i++) {
+            auto p = pq.top();
+            pq.pop();
+
+            result = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+
+            if(col + 1 < n) {
+                pq.push({matrix[row][col+1], {row, col+1}});
             }
         }
 
-        return pq.top();
+        return result;
     }
 };
